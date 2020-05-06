9960c2c6123953d245d8335f5404a36fe860dba3
CazPur <caspervanhal@gmail.com>
2020-05-06 14:47:50 +0200

    Initial commit

---
+++ Hallo1.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Hallo1
+{
+    static void Main()
+    {
+        Console.WritLine("Hallo!");
+        Console.Readline();
+    }
+}
---
+++ HalloCSharp.cs
@@ -0,0 +1,7 @@
+class HalloCSharp
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Hello C#!");
+    }
+}
